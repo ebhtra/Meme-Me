f7c5a54f746025e492ad9216693d29352e6dfd98
ethan <dirtdirtdirtdirt@aol.com>
2015-05-19 12:18:52 -0400

    Initial Commit

---
+++ Meme Me/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Meme Me
+//
+//  Created by Ethan Haley on 5/19/15.
+//  Copyright (c) 2015 Ethan Haley. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
